d51e7b7226ff2bff4f4c6599a7d158d53e8643de
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-12-03 05:25:49 -0500

    Generalize create_localized_shared_lib a bit

--- cmake/Modules/localization.cmake
+++ cmake/Modules/localization.cmake
@@ -1,28 +1,54 @@
-function (generate_symbol_list FROM_TARGET)
-    add_custom_command(OUTPUT ${FROM_TARGET}.nm
-            DEPENDS ${FROM_TARGET}
-            COMMAND nm -g --defined-only -B $<TARGET_FILE:${FROM_TARGET}> | cut -d " " -f 3 >${FROM_TARGET}.nm
-            WORKING_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
-            COMMENT "Generating list of all symbols for $<TARGET_FILE:${FROM_TARGET}>.")
+function (make_valid_filename FROM_STRING OUT_NAME)
+    string(REPLACE "::" "_" NAME "${FROM_STRING}")
+    set(${OUT_NAME} ${NAME} PARENT_SCOPE)
 endfunction()
 
-function (create_localized_shared_lib TARGET DEP_LOCALIZE SOURCES)
-    add_library(${TARGET}r OBJECT ${SOURCES})
-    set_target_properties(${TARGET}r PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
-    set_target_properties(${TARGET}r PROPERTIES LINK_FLAGS "-v")
+function (generate_symbol_list FROM_TARGET)
+    if (NOT MSVC)
+        make_valid_filename(${FROM_TARGET} FROM_TARGET_FILENAME)
+        add_custom_command(OUTPUT ${FROM_TARGET_FILENAME}.nm
+                DEPENDS ${FROM_TARGET}
+                COMMAND nm -g --defined-only -B $<TARGET_FILE:${FROM_TARGET}> | cut -d " " -f 3 | grep -v "gxx_personality" >${FROM_TARGET_FILENAME}.nm
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                COMMENT "Generating list of all symbols for $<TARGET_FILE:${FROM_TARGET}>.")
 
-    add_custom_command(OUTPUT lib${TARGET}i.a
-            DEPENDS ${TARGET}r ${DEP_LOCALIZE} ${DEP_LOCALIZE}.nm
-            COMMAND ${CMAKE_LINKER} -Ur $<TARGET_OBJECTS:${TARGET}r> $<TARGET_FILE:${DEP_LOCALIZE}> -o lib${TARGET}i.a
-            COMMAND ${CMAKE_OBJCOPY} -v --localize-symbols ${DEP_LOCALIZE}.nm lib${TARGET}i.a
-            WORKING_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
-            COMMENT "Localizing all symbols from ${DEP_LOCALIZE} in lib${TARGET}i.a.")
+        add_custom_target(
+                ${FROM_TARGET_FILENAME}_nm
+                DEPENDS
+                    ${FROM_TARGET_FILENAME}.nm)
+    endif()
+endfunction()
 
-    add_library(${TARGET}i INTERFACE)
-    set_target_properties(${TARGET}i PROPERTIES INTERFACE_SOURCES ${CMAKE_CURRENT_BINARY_DIR}/lib${TARGET}i.a)
-    set_target_properties(${TARGET}i PROPERTIES INTERFACE_LINK_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/lib${TARGET}i.a)
+function (create_localized_shared_lib TARGET TYPE DEP_LOCALIZE OUT_SRC_TARGET_VAR)
+    if (MSVC)
+        add_library(${TARGET} ${TYPE})
+        set(${OUT_SRC_TARGET_VAR} ${TARGET} PARENT_SCOPE)
+    else ()
+        set(${OUT_SRC_TARGET_VAR} ${TARGET}r PARENT_SCOPE)
 
-    add_library(${TARGET} SHARED dummy.cpp)
-    target_link_libraries(${TARGET} PRIVATE ${TARGET}i $<TARGET_PROPERTY:${DEP_LOCALIZE},LINK_LIBRARIES>)
-    set_target_properties(${TARGET} PROPERTIES LINK_FLAGS "-v")
+        add_library(${TARGET}r OBJECT)
+        set_target_properties(${TARGET}r PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+
+        make_valid_filename(${DEP_LOCALIZE} DEP_LOCALIZE_FILENAME)
+        add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/lib${TARGET}i.a
+                DEPENDS ${TARGET}r ${DEP_LOCALIZE} ${DEP_LOCALIZE_FILENAME}_nm
+                COMMAND ${CMAKE_LINKER} -Ur $<TARGET_OBJECTS:${TARGET}r> $<TARGET_FILE:${DEP_LOCALIZE}> -o lib${TARGET}i.a
+                COMMAND ${CMAKE_OBJCOPY} -v --localize-symbols ${CMAKE_BINARY_DIR}/${DEP_LOCALIZE_FILENAME}.nm lib${TARGET}i.a
+                COMMAND_EXPAND_LISTS
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMENT "Localizing all symbols from ${DEP_LOCALIZE} in lib${TARGET}i.a.")
+        add_custom_target(${TARGET}it DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/lib${TARGET}i.a)
+
+        add_library(${TARGET}i STATIC IMPORTED)
+        add_dependencies(${TARGET}i PRIVATE ${TARGET}it)
+        target_link_libraries(${TARGET}i INTERFACE $<TARGET_PROPERTY:${TARGET}r,LINK_LIBRARIES>)
+        set_target_properties(${TARGET}i PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/lib${TARGET}i.a)
+
+        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp "")
+
+        add_library(${TARGET} ${TYPE})
+        target_sources(${TARGET} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp)
+        # TODO shouldn't this be INTERFACE_LINK_LIBRARIES?
+        target_link_libraries(${TARGET} PRIVATE ${TARGET}i $<TARGET_PROPERTY:${DEP_LOCALIZE},LINK_LIBRARIES>  )
+    endif()
 endfunction()
